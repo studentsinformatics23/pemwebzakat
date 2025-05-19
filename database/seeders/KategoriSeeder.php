481db8ef947f41021171c76c748a95d7654c9d9f
raihannurhidayat <grandrx041@gmail.com>
2025-05-19 19:28:57 +0700

    feat: add category

---
+++ database/seeders/KategoriSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Kategori;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class KategoriSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $kategori = [
+            ['name' => 'Fakir', 'desc' => 'Orang yang hampir tidak memiliki apa-apa dan hidup dalam kemiskinan.'],
+            ['name' => 'Miskin', 'desc' => 'Orang yang memiliki harta tetapi tidak cukup untuk memenuhi kebutuhan dasar.'],
+            ['name' => 'Amil', 'desc' => 'Petugas yang mengumpulkan dan mendistribusikan zakat.'],
+            ['name' => 'Muallaf', 'desc' => 'Orang yang baru masuk Islam dan membutuhkan bantuan untuk menguatkan iman.'],
+            ['name' => 'Riqab', 'desc' => 'Budak atau hamba sahaya yang ingin memerdekakan diri.'],
+            ['name' => 'Gharim', 'desc' => 'Orang yang memiliki hutang untuk kebutuhan hidup yang halal dan tidak mampu membayarnya.'],
+            ['name' => 'Fisabilillah', 'desc' => 'Orang yang berjuang di jalan Allah, termasuk kegiatan dakwah, pendidikan, dan jihad.'],
+            ['name' => 'Ibnu Sabil', 'desc' => 'Musafir yang kehabisan biaya dalam perjalanan.'],
+        ];
+
+
+        foreach ($kategori as $item) {
+            Kategori::insert([
+
+                'nama' => $item['name'],
+                'deskripsi' => $item['desc'],
+            ]);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Foundation\Application;
@@ -15,17 +16,14 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 Route::middleware(['auth'])->group(function () {
+    Route::get("/dashboard", [DashboardController::class, "index"])->name("dashboard");
+    Route::get("/bayar", function () {
+        return Inertia::render('bayar');
+    })->name("dashboard");
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/bayar', function(){
-    return Inertia::render('bayar');
-    })->middleware(['auth'])->name('bayar');
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
